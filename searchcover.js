f468250deb1265c2d28560e6b1dff672cddc15d0
Rosanne Hu <rhu2@wellesley.edu>
2017-02-11 18:45:12 -0500

    added the searchcover.js

---
+++ searchcover.js
@@ -0,0 +1,7 @@
+/*
+Author: Rosanne Hu
+Filename: searchcover.js
+functionality: The purpose of this is listen to the browser and go through
+the the parsed text and recognize the keywords given through user input
+and determine whether or not we will cover it.
+*/
