3eb4358aa69a4c4a1d924d2f55531850a4a7aa71
Rosanne Hu <rhu2@wellesley.edu>
2017-02-12 08:45:23 -0500

    asnuisnj

--- searchcover.js
+++ searchcover.js
@@ -7,11 +7,13 @@ and determine whether or not we will cover it.
 */
 
 //takes in the black listed words from the popup .js
-var triggers = ["cats"];
+var triggers = blacklisted;//from
 //the selector for the class containing post posts that
 //we need to access
 var postclass = '_5pbx userContent';
 var selectors = {};
+var triggers;
+var k=0;
 
 
 
@@ -21,10 +23,23 @@ function searchFile(){
     for(var i=0; i<triggers.length;i++){
     //looks through the .5pbx.userContent div and finds all the divs
     //containing the word(returns array of selectors)
-    selectors[triggers[i]]($( "._5pbx.userContent" ).find("p:contains(triggers[i])"));
-    //puts all the paragraphs into dictionary with taga
-    
+    trigger++;
+    var arrays = $( "._5pbx.userContent" ).find("p:contains(triggers[i])");
+    for(var j=0;j<arrays.length;j++){
+      $(arrays[i]).id= "cwarn" +k;
+      k++;
+    }
+    }
 
+    for(var i=0; i<)
+    //i need to put a pointer to the elemtn
+    //if element
+    //give the specific paragarph an id and set visibility hidden
+    //on page put a div on top. and when
+    //sibiling element htat is a paragtaph that i am inserting
+    //when click on the element div that we have than we can but
+    selectors[triggers[i]](arrays);
+    //puts all the paragraphs into dictionary with tags
   }
 
 }
